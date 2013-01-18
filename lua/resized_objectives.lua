5ecdbf7f8e3d404e8f896ef70cd05851de5d61aa
mattsc <mschoeck@tmt.org>
2013-01-18 06:47:54 -0800

    Add handling for [show_objectives]

--- lua/resized_objectives.lua
+++ lua/resized_objectives.lua
@@ -22,3 +22,26 @@ function wesnoth.wml_actions.objectives(cfg)
         set_objectives(sides)
     end
 end
+
+local old_show_objectives = wesnoth.wml_actions.show_objectives
+function wesnoth.wml_actions.show_objectives(cfg)
+    local parsed_cfg = cfg.__parsed
+
+    local sides = wesnoth.get_sides(parsed_cfg)
+
+    old_show_objectives(cfg)
+
+    local function set_objectives(sides)
+        for i, team in ipairs(sides) do
+            local objectives = team.objectives
+            objectives = "<span font='" .. objectives_size .. "'>" .. objectives .. "</span>"
+            team.objectives = objectives
+        end
+    end
+
+    if #sides == #wesnoth.sides or #sides == 0 then
+        set_objectives(wesnoth.sides)
+    else
+        set_objectives(sides)
+    end
+end
